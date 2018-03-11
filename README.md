dc8acaf25c4d5d6f6c4b46cd12e561da51145412
scarescrow <sagnik.sinharoy@gmail.com>
2018-03-11 16:36:28 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## Starting App for Electron
+
+This is a classic Shopping list app made for beginners to start on Electron JS.
+App has been made from [this tutorial](https://www.youtube.com/watch?v=kN1Czs0m1SU)
+
+#### Steps to start development
+
+* Clone the project
+* Run command ````npm install```` 
+* Make changes in code
+* Run command ````npm start````
+
+#### Steps to build production application
+
+* Set ENV for app to production (line #9 in main.js)
+* Run either of the following commands depending on system OS:
+	- npm run package-linux
+	- npm run package-win
+	- npm run package-mac
+* Executable will be present in release-builds folder
+
+## LICENSE
+
+Licensed under [MIT License](LICENSE)
---
+++ main.js
@@ -0,0 +1,133 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+// SET ENV
+
+// process.env.NODE_ENV = 'production';
+
+let mainWindow;
+let addWindow;
+
+// Listen for the app to be ready
+
+app.on('ready', function() {
+	// Create new Window
+
+	mainWindow = new BrowserWindow({});
+
+	// Load html into window
+
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'mainWindow.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+
+	// Quit App when closed
+
+	mainWindow.on('closed', function() {
+		app.quit();
+	});
+
+	// Build menu from template
+
+	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+	// Insert Menu
+
+	Menu.setApplicationMenu(mainMenu)
+});
+
+// Handle create add window
+
+function createAddWindow() {
+	// Create add Window
+
+	addWindow = new BrowserWindow({
+		width: 300,
+		height: 200,
+		title: 'Add Shopping List Item'
+	});
+
+	addWindow.setMenu(null)
+
+	// Load html into window
+
+	addWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'addWindow.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+
+	// Garbabe collection handle
+
+	addWindow.on('close', function() {
+		addWindow = null;
+	});
+}
+
+// Catch item add
+
+ipcMain.on('item:add', function(e, item) {
+	mainWindow.webContents.send('item:add', item);
+	addWindow.close();
+});
+
+// Create menu template
+
+const mainMenuTemplate = [
+	{
+		label: 'File',
+		submenu: [
+			{
+				label: 'Add Item',
+				accelerator: process.platform == 'darwin' ? 'Command+D' : 'Ctrl+D',
+				click() {
+					createAddWindow();
+				}
+			},
+			{
+				label: 'Clear Items',
+				click() {
+					mainWindow.webContents.send('item:clear');
+				}
+			},
+			{
+				label: 'Quit',
+				accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+				click() {
+					app.quit()
+				}
+			}
+		]
+	}
+];
+
+// If mac, add empty object to menu
+
+if (process.platform == "darwin") {
+	mainMenuTemplate.unshift({});
+}
+
+// Add developer tools item if not in production
+
+if(process.env.NODE_ENV !== 'production') {
+	mainMenuTemplate.push({
+		label: 'Developer Tools',
+		submenu: [
+			{
+				label: 'Toggle Dev Tools',
+				accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+				click(item, focussedWindow) {
+					focussedWindow.toggleDevTools();
+				}
+			},
+			{
+				role: 'reload',
+			}
+		]
+	});
+}
